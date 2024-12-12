0fe8fc4f74a9e8960e3eb6cd125d5d0deb781eb9
Naung Ye Htet <naungyehtet717@gmail.com>
2024-12-12 14:02:19 +0630

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cameraRoutes from "./routes/cameraRoutes";
+import cameraGroupRoutes from "./routes/cameraGroupRoutes";
 import sequelize from "./config/database";
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api", cameraRoutes);
+app.use("/api", cameraGroupRoutes);
 
 // Database connection
 sequelize.sync().then(() => console.log("Database connected"));
--- backend/src/routes/cameraGroupRoutes.ts
+++ backend/src/routes/cameraGroupRoutes.ts
@@ -1,31 +1,8 @@
-import express, { Request, Response } from "express";
-import {
-  createGroup,
-  getAllGroups,
-  // addCameraToGroup,
-  // removeCameraFromGroup,
-  // getCamerasInGroup,
-  // deleteGroup,
-} from "../controllers/cameraGroupController";
+import express from "express";
+import { getAllGroups } from "../controllers/cameraGroupController";
 
 const router = express.Router();
 
-router.post("/groups", async (req: Request, res: Response) =>
-  createGroup(req, res)
-);
-router.get("/groups", async (req: Request, res: Response) =>
-  getAllGroups(req, res)
-);
-// router.post("/groups/add-camera", async (req: Request, res: Response) =>
-//   addCameraToGroup(req, res)
-// );
-// router.post("/groups/remove-camera", async (req: Request, res: Response) =>
-//   removeCameraFromGroup(req, res)
-// );
-// router.get("/groups/:groupId/cameras", async (req: Request, res: Response) =>
-//   getCamerasInGroup(req, res)
-// );
-// router.delete("/groups/:groupId", async (req: Request, res: Response) =>
-//   deleteGroup(req, res)
-// );
+router.get("/groups", getAllGroups);
+
 export default router;
